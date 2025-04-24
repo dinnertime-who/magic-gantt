ac3b3eda0ea1abf1f5d0113ce58de1d3b9454693
dinnertime <dinnertime.dev@gmail.com>
2025-04-24 11:03:46 +0900

    변경: 패키지 이름을 'frappe-gantt'에서 'magic-gantt'로 변경하고, 관련된 파일 이름 및 경로 수정

--- vite.config.js
+++ vite.config.js
@@ -6,16 +6,16 @@ export default defineConfig({
         lib: {
             entry: resolve(__dirname, 'src/index.js'),
             name: 'Gantt',
-            fileName: 'frappe-gantt',
+            fileName: 'magic-gantt',
         },
         rollupOptions: {
             output: {
                 format: 'cjs',
-                assetFileNames: 'frappe-gantt[extname]',
-                entryFileNames: 'frappe-gantt.[format].js'
+                assetFileNames: 'magic-gantt[extname]',
+                entryFileNames: 'magic-gantt.[format].js',
             },
         },
     },
     output: { interop: 'auto' },
-    server: { watch: { include: ['dist/*', 'src/*'] } }
+    server: { watch: { include: ['dist/*', 'src/*'] } },
 });
